cca0000bad6f70baa2fa67160db988f01a779af2
emilbek-abdyrazakov <emil_kg@yahoo.com>
2023-10-04 22:00:02 +0000

    change1

--- main.tf
+++ main.tf
@@ -1,19 +1,5 @@
-data "aws_ami" "filter" {
-  most_recent = false
-  owners      = ["amazon"]
-
-  filter {
-    name   = "image-id"
-    values = ["ami-067d1e*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "myec2" {
-  ami           = data.aws_ami.filter.id
+  ami           = "ami-036f5574583e16426"
   instance_type = "t2.nano"
   count         = 2
   tags = {
